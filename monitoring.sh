9b345eafe3ade2a4b7563e0aeb466ba0698cd91b
Dilshan Wijesooriya <dilshanonline@users.noreply.github.com>
2020-05-08 01:00:53 +0530

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -3,11 +3,11 @@
 #- Linux CPU and Memory Monitor
 #- Author : Dilshan Wijesooriya
 
+while [ 1 ]; do
 cpu_load=$(top -b -n  1  | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }')
-cpu_load_last1min=$(top -b -n  1  | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }' | cut -d ' ' -f1)
-cpu_load_last5min=$(top -b -n  1  | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }' | cut -d ' ' -f2)
-cpu_load_last15min=$(top -b -n  1  | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }' | cut -d ' ' -f3)
-c_time=$(date +%Y.%m.%d-%H:%M)
+cpu_load_last1min=$(top -b -n  1  | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }' | cut -d ' ' -f2)
+cpu_load_last5min=$(top -b -n  1  | awk '/load average/ { printf "%s %s %s\n", $10, $11, $12 }' | cut -d ' ' -f3)
+c_time=$(date +%Y.%m.%d-%H:%M:%S)
 total_mem=$(free -g | grep ^Mem | tr -s ' ' | cut -d ' ' -f2)
 used_mem=$(free -g | grep ^Mem | tr -s ' ' | cut -d ' ' -f3)
 free_mem=$(free -g | grep ^Mem | tr -s ' ' | cut -d ' ' -f4)
@@ -15,15 +15,19 @@ cached_mem=$(free -g | grep ^Mem | tr -s ' ' | cut -d ' ' -f6)
 echo '------------------------------'
 echo 'Timestamp: ' $c_time
 echo '------------------------------'
-echo '-----CPU Load-----'
+echo '-----VM CPU Load-----'
 echo 'Load for Last 01 Minutes: '$cpu_load_last1min
 echo 'Load for Last 05 Minutes: '$cpu_load_last5min
-echo 'Load for Last 15 Minutes: '$cpu_load_last15min
 echo '------------------------------'
-echo '----Memory Usage----'
+echo '---------usf stat-------------'
+docker ps -q | xargs  docker stats --no-stream | grep -B3 'azureuser_usf_1'
+echo '------------------------------'
+echo '----VM Memory Usage----'
 echo 'Total : ' $total_mem
 echo 'Used  : ' $used_mem
 echo 'Free  : ' $free_mem
 echo 'Cache : ' $cached_mem
 echo '------------------------------'
 echo " "
+sleep 15
+done
